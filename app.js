d6e656b2aea6e6cccec387324619fb7d88512bb5
Garrick-Clarke <garricksclarke@gmail.com>
2021-06-01 01:20:59 -0400

    change html name to index.

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const hostname = '127.0.0.1';
 const port = 3000;
 
 const server = http.createServer((req, res) => {
-    fs.readFile('TranscriptToGPA.html', function(err, data) {
+    fs.readFile('index.html', function(err, data) {
         res.writeHead(200, {'Content-Type': 'text/html'});
         res.write(data)
         return res.end();
